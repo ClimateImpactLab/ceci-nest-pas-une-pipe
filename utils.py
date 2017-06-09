3b3a6b948f02c70a7283a4505d4f953615c0c424
Michael Delgado <mdelgado@rhg.com>
2017-06-08 19:10:07 -0700

    rcp45 pattern climate job

---
+++ utils.py
@@ -0,0 +1,94 @@
+import os
+import click
+import itertools
+import functools
+
+SLURM_SCRIPT = '''
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=resid
+#
+# Partition:
+#SBATCH --partition=savio2
+#
+# Account:
+#SBATCH --account=co_laika
+#
+# QoS:
+#SBATCH --qos=savio_lowprio
+#
+#SBATCH --nodes=1
+#
+#SBATCH --ntasks-per-node=24
+#
+#SBATCH  --cpus-per-task=1
+#
+# Wall clock limit:
+#SBATCH --time=72:00:00
+#
+#SBATCH --array=0-{numjobs}
+
+## Run command
+python {filepath} --job_id ${{SLURM_ARRAY_TASK_ID}}
+'''.strip()
+
+
+def _product(values):
+    '''
+    Examples
+    --------
+
+    .. code-block:: python
+
+        >>> _product([3, 4, 5])
+        60
+
+    '''
+    return functools.reduce(lambda x, y: x*y, values, 1)
+
+
+def _unpack_job(specs):
+    job = {}
+    for spec in specs:
+        job.update(spec)
+    return job
+
+
+def generate_jobs(job_spec):
+    for specs in itertools.product(*job_spec):
+        yield _unpack_job(specs)
+
+
+def _prep_slurm(filepath, job_spec):
+    with open('do_job.sh', 'w+') as f:
+        f.write(SLURM_SCRIPT.format(
+            numjobs = len(list(generate_jobs(job_spec))),
+            filepath = filepath))
+
+
+def run_with_slurm(filepath, job_spec):
+    _prep_slurm(filepath, job_spec)
+    os.system('sbatch do_job.sh')
+
+
+def get_job_by_index(job_spec, index):
+    '''
+    Examples
+    --------
+
+    .. code-block:: python
+
+        >>> get_job_by_index(
+        ...    (('a', 'b', 'c'), (1, 2, 3), ('do', 'rey', 'mi')), 5)
+        ('a', 2, 'mi')
+
+        >>> get_job_by_index(map(tuple, ['hi', 'hello', 'bye']), 10)
+        ('h', 'l', 'y')
+
+
+    '''
+
+    return _unpack_job([
+        job_spec[i][
+            (index//(_product(map(len, job_spec[i+1:])))%len(job_spec[i]))]
+        for i in range(len(job_spec))])
---
+++ web_seasonal_temp.py
@@ -0,0 +1,184 @@
+import os
+import click
+import pprint
+import logging
+import xarray as xr
+import pandas as pd
+
+import utils
+from climate_toolbox import (
+    load_bcsd,
+    load_baseline,
+    weighted_aggregate_grid_to_regions)
+
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT)
+
+logger = logging.getLogger('uploader')
+logger.setLevel('DEBUG')
+
+__author__ = 'Michael Delgado'
+__contact__ = 'mdelgado@rhg.com'
+__version__ = '0.1.0'
+
+BASELINE_FILE = (
+    '/global/scratch/jiacany/nasa_bcsd/pattern/baseline/' +
+    '{baseline_model}/{variable}/' +
+    '{variable}_baseline_1986-2005_r1i1p1_{baseline_model}_{season}.nc')
+
+BCSD_pattern_files = (
+    '/global/scratch/mdelgado/nasa_bcsd/pattern/SMME_surrogate/' +
+    '{rcp}/{variable}/{model}/' +
+    '{variable}_BCSD_{model}_{rcp}_r1i1p1_{season}_{{year}}.nc')
+
+WRITE_PATH = (
+    '/global/scratch/mdelgado/web/gcp/climate/{rcp}/{agglev}/{variable}/' +
+    '{variable}_{agglev}_{aggwt}_{model}_{season}_{pername}.nc')
+
+ADDITIONAL_METADATA = dict(
+    description=__file__.__doc__,
+    author=__author__,
+    contact=__contact__,
+    version=__version__,
+    repo='https://github.com/ClimateImpactLab/pipelines',
+    file='/pipelines/climate/jobs/impactlab_website/job_pattern_bcsd_ir_slurm.py',
+    execute='job_pattern_bcsd_ir_slurm.job_pattern_bcsd_ir_slurm.run_slurm()',
+    project='gcp', 
+    team='climate',
+    geography='hierid',
+    weighting='areawt',
+    frequency='20yr')
+
+
+def average_seasonal_temp_pattern(ds):
+    '''
+    Average seasonal tas
+    '''
+    return (ds.tas - 273.15).mean(dim='day')
+
+
+JOBS = [
+    dict(variable='tas', transformation=average_seasonal_temp_pattern)]
+
+PERIODS = [
+    dict(rcp='rcp45', pername='2020', years=list(range(2020, 2040))),
+    dict(rcp='rcp45', pername='2040', years=list(range(2040, 2060))),
+    dict(rcp='rcp45', pername='2080', years=list(range(2080, 2100)))]
+
+YEARS = []
+
+MODELS = list(map(lambda x: dict(model=x[0], baseline_model=x[1]), [
+        ('pattern1','MRI-CGCM3'),
+        ('pattern2','GFDL-ESM2G'),
+        ('pattern3','MRI-CGCM3'),
+        ('pattern4','GFDL-ESM2G'),
+        ('pattern5','MRI-CGCM3'),
+        ('pattern6','GFDL-ESM2G'),
+        ('pattern28','GFDL-CM3'),
+        ('pattern29','CanESM2'),
+        ('pattern30','GFDL-CM3'),
+        ('pattern31','CanESM2'), 
+        ('pattern32','GFDL-CM3'), 
+        ('pattern33','CanESM2')]))
+
+SEASONS = list(map(lambda x: dict(season=x),[ 'DJF', 'MAM', 'JJA', 'SON']))
+
+AGGREGATIONS = [
+    {'agglev': 'ISO', 'aggwt': 'areawt'},
+    {'agglev': 'hierid', 'aggwt': 'areawt'}]
+
+
+JOB_SPEC = [JOBS, PERIODS, MODELS, SEASONS, AGGREGATIONS]
+
+def run_job(
+        metadata,
+        variable,
+        transformation,
+        rcp,
+        pername,
+        years,
+        model,
+        baseline_model,
+        season,
+        agglev,
+        aggwt,
+        weights=None):
+
+    # Add to job metadata
+    metadata.update(dict(
+        time_horizon='{}-{}'.format(years[0], years[-1])))
+
+    baseline_file = BASELINE_FILE.format(**metadata)
+    pattern_file = BCSD_pattern_files.format(**metadata)
+    write_file = WRITE_PATH.format(**metadata)
+
+    # Get transformed data
+    total = []
+
+    for year in years:
+
+        # Get transformed data
+        pattf = pattern_file.format(year=year)
+        logger.debug('attempting to load pattern file: {}'.format(pattf))
+        annual = load_bcsd(pattf, variable, broadcast_dims=('day',))
+        
+        logger.debug('{} {} - applying transform'.format(model, year))
+        annual = xr.Dataset({
+            variable: annual.pipe(transformation)})
+
+        logger.debug('{} {} - adding to running total'.format(model, year))
+        total.append(annual)
+
+    ds = xr.concat(total, dim=pd.Index(years, name='year')).mean(dim='year')
+
+    # load baseline
+    logger.debug('attempting to load baseline file: '.format(baseline_file))
+    base = load_baseline(baseline_file, variable)
+
+    logger.debug('{} - adding pattern residuals to baseline'.format(model))
+    ds = (ds + base)
+
+    # Reshape to regions
+    logger.debug('{} - reshaping to regions'.format(model))
+    if not agglev.startswith('grid'):
+        ds = weighted_aggregate_grid_to_regions(
+                ds, variable, aggwt, agglev, weights=weights)
+
+    # Update netCDF metadata
+    logger.debug('{} udpate metadata'.format(model))
+    ds.attrs.update(**metadata)
+
+    # Write output
+    logger.debug('attempting to write to file: {}'.format(write_file))
+    if not os.path.isdir(os.path.dirname(write_file)):
+        os.makedirs(os.path.dirname(write_file))
+
+    ds.to_netcdf(write_file)
+
+
+@click.command()
+@click.option('--prep', is_flag=True, default=False)
+@click.option('--run', is_flag=True, default=False)
+@click.option('--job_id', type=int, default=None)
+def main(prep=False, run=False, job_id=None):
+    if prep:
+        utils._prep_slurm(filepath=__file__, job_spec=JOB_SPEC)
+
+    elif run:
+        utils.run_slurm(filepath=__file__, job_spec=JOB_SPEC)
+
+    if job_id is not None:
+        job = utils.get_job_by_index(JOB_SPEC, job_id)
+
+        logger.debug('Beginning job\nid:\t{}\nkwargs:\t{}'.format(
+            job_id,
+            pprint.pformat(job, indent=2)))
+
+        metadata = {k: v for k, v in ADDITIONAL_METADATA.items()}
+        metadata.update(job)
+
+        run_job(metadata=metadata, **job)
+
+
+if __name__ == '__main__':
+    main()
