91f3c798fd4bd07992d9d127d0a3447145175720
jgerardsimcock <justin_simcock@yahoo.com>
2017-06-09 15:22:42 -0700

    add tasmax_tasmin

--- tasmax_tasmin.py
+++ tasmax_tasmin.py
@@ -21,19 +21,13 @@ __author__ = 'Michael Delgado'
 __contact__ = 'mdelgado@rhg.com'
 __version__ = '0.1.0'
 
-BASELINE_FILE = (
-    '/global/scratch/jsimcock/gcp/climate/hierid/' +
-    '{rcp}/tas/tas-polynomials/' +
-    'tas_tas-polynomials_{baseline_model}_{year}.nc')
+BCSD_orig_files = os.path.join(
+    '/global/scratch/jiacany/nasa_bcsd/raw_data/{rcp}/{model}/{variable}',
+    '{variable}_day_BCSD_{rcp}_r1i1p1_{model}_{{year}}.nc')
 
-# BCSD_pattern_files = (
-#     '/global/scratch/mdelgado/nasa_bcsd/pattern/SMME_surrogate/' +
-#     '{rcp}/{variable}/{model}/' +
-#     '{variable}_BCSD_{model}_{rcp}_r1i1p1_{season}_{{year}}.nc')
-
-WRITE_PATH = (
-    '/global/scratch/jsimcock/gcp/climate/hierid/{rcp}/{variable}/tas-polynomials-C/' +
-    'tas_tas-polynomials_{model}_{year}.nc')
+WRITE_PATH = os.path.join(
+    '/global/scratch/jsimcock/gcp/climate/{agglev}/{rcp}/{variable}/{transformation}',
+    '{variable}_{agglev}_{aggwt}_{model}_{pername}.nc')
 
 ADDITIONAL_METADATA = dict(
     description=__file__.__doc__,
@@ -41,55 +35,73 @@ ADDITIONAL_METADATA = dict(
     contact=__contact__,
     version=__version__,
     repo='https://github.com/ClimateImpactLab/ceci-nest-pas-une-pipe',
-    file='/ceci-nest-pas-une-pipe/',
-    execute='job_pattern_bcsd_ir_slurm.job_pattern_bcsd_ir_slurm.run_slurm()',
+    file='/ceci-nest-pas-une-pipe/tasmax_tasmin.py',
+    execute='',
     project='gcp', 
     team='climate',
-    geography='hierid',
-    weighting='popwt',
-    frequency='daily')
+    geography='',
+    weighting='areawt',
+    frequency='20 year')
 
 
-def average_seasonal_temp_pattern(ds):
+def tasmax_over_95F(ds):
     '''
     Average seasonal tas
     '''
-    return (ds.tas - 273.15).mean(dim='day')
+    
+    return ds.tasmax.where((ds.tasmax - 273.15) > 35).count(dim='time')
+
+def tasmin_under_32F(ds):
+    '''
+    Count of days with tasmin under 32F/0C
+    '''
+    return ds.tasmin.where((ds.tasmin- 273.15) < 0).count(dim='time')
 
 
 JOBS = [
-    dict(variable='tas', transformation=average_seasonal_temp_pattern)]
+    dict(variable='tasmax', transformation=tasmax_over_95F), 
+    dict(variable='tasmax', transformation=tasmin_under_32F)
+    ] 
+
 
 PERIODS = [
-    dict(rcp='historical', pername='1986', years=list(range(1986, 2006))),
     dict(rcp='rcp45', pername='2020', years=list(range(2020, 2040))),
     dict(rcp='rcp45', pername='2040', years=list(range(2040, 2060))),
     dict(rcp='rcp45', pername='2080', years=list(range(2080, 2100)))]
 
+
 YEARS = []
 
-MODELS = list(map(lambda x: dict(model=x[0], baseline_model=x[1]), [
-        ('pattern1','MRI-CGCM3'),
-        ('pattern2','GFDL-ESM2G'),
-        ('pattern3','MRI-CGCM3'),
-        ('pattern4','GFDL-ESM2G'),
-        ('pattern5','MRI-CGCM3'),
-        ('pattern6','GFDL-ESM2G'),
-        ('pattern28','GFDL-CM3'),
-        ('pattern29','CanESM2'),
-        ('pattern30','GFDL-CM3'),
-        ('pattern31','CanESM2'), 
-        ('pattern32','GFDL-CM3'), 
-        ('pattern33','CanESM2')]))
+MODELS = list(map(lambda x: dict(model=x), [
+    'ACCESS1-0',
+    'bcc-csm1-1',
+    'BNU-ESM',
+    'CanESM2',
+    'CCSM4',
+    'CESM1-BGC',
+    'CNRM-CM5',
+    'CSIRO-Mk3-6-0',
+    'GFDL-CM3',
+    'GFDL-ESM2G',
+    'GFDL-ESM2M',
+    'IPSL-CM5A-LR',
+    'IPSL-CM5A-MR',
+    'MIROC-ESM-CHEM',
+    'MIROC-ESM',
+    'MIROC5',
+    'MPI-ESM-LR',
+    'MPI-ESM-MR',
+    'MRI-CGCM3',
+    'inmcm4',
+    'NorESM1-M']))
 
-SEASONS = list(map(lambda x: dict(season=x),[ 'DJF', 'MAM', 'JJA', 'SON']))
 
 AGGREGATIONS = [
     {'agglev': 'ISO', 'aggwt': 'areawt'},
     {'agglev': 'hierid', 'aggwt': 'areawt'}]
 
 
-JOB_SPEC = [JOBS, PERIODS, MODELS, SEASONS, AGGREGATIONS]
+JOB_SPEC = [JOBS, PERIODS, MODELS, AGGREGATIONS]
 
 def run_job(
         metadata,
@@ -109,8 +121,7 @@ def run_job(
     metadata.update(dict(
         time_horizon='{}-{}'.format(years[0], years[-1])))
 
-    baseline_file = BASELINE_FILE.format(**metadata)
-    pattern_file = BCSD_pattern_files.format(**metadata)
+    bcsd_file = BCSD_orig_files.format(**metadata)
     write_file = WRITE_PATH.format(**metadata)
 
     # Get transformed data
@@ -118,10 +129,13 @@ def run_job(
 
     for year in years:
 
+
+        
+
         # Get transformed data
-        pattf = pattern_file.format(year=year)
-        logger.debug('attempting to load pattern file: {}'.format(pattf))
-        annual = load_bcsd(pattf, variable, broadcast_dims=('day',))
+        bcsd_file = bcsd_file.format(year=year)
+        logger.debug('attempting to load pattern file: {}'.format(bcsd_file))
+        annual = load_bcsd(bcsd_file, variable, broadcast_dims=('time',))
         
         logger.debug('{} {} - applying transform'.format(model, year))
         annual = xr.Dataset({
@@ -134,10 +148,7 @@ def run_job(
 
     # load baseline
     logger.debug('attempting to load baseline file: '.format(baseline_file))
-    base = load_baseline(baseline_file, variable)
 
-    logger.debug('{} - adding pattern residuals to baseline'.format(model))
-    ds = (ds + base)
 
     # Reshape to regions
     logger.debug('{} - reshaping to regions'.format(model))
