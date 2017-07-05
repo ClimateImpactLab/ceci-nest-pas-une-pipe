21241ddadcd3d940c24d13cd74bfed77470cd868
Michael Delgado <mdelgado@rhg.com>
2017-07-05 12:49:47 -0700

    proj2 only

--- team_poly_bscd.py
+++ team_poly_bscd.py
@@ -148,14 +148,17 @@ JOBS = [
 
 hist = range(1981, 2006)
 proj1 = range(2006, 2050)
-proj2 = range(2050, 2100)
+proj2 = range(2050, 2080)
+proj3 = range(2080, 2100)
 
 PERIODS = (
     # [dict(scenario='historical', read_acct='jiacany', year=y) for y in hist] +
     [dict(scenario='rcp45', read_acct='jiacany', year=y) for y in proj1] +
     # [dict(scenario='rcp45', read_acct='jiacany', year=y) for y in proj2] +
+    # [dict(scenario='rcp45', read_acct='jiacany', year=y) for y in proj3] +
     # [dict(scenario='rcp85', read_acct='jiacany', year=y) for y in proj1] +
     # [dict(scenario='rcp85', read_acct='jiacany', year=y) for y in proj2] +
+    # [dict(scenario='rcp85', read_acct='jiacany', year=y) for y in proj3] +
     [])
 
 MODELS = list(map(lambda x: dict(model=x), [
