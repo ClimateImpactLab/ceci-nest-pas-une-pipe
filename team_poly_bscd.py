2c6b40b25ce2d943a68942af0a5defe412e6a7ef
Michael Delgado <mdelgado@rhg.com>
2017-07-05 12:20:15 -0700

    bring in team_poly_bcsd from parallel

--- team_poly_bscd.py
+++ team_poly_bscd.py
@@ -1,25 +1,17 @@
 '''
 Powers of daily average temperature
 
-Values are daily mean temperature raised to various powers for use in 
+Values are daily mean temperature raised to various powers for use in
 polynomial response functions, aggregated to impact regions/hierids using
 population weights. Data is reported at the daily level using a 365-day
 calendar (leap years excluded) in the format YYYYDDD.
 '''
 
 import os
-import click
 import pprint
 import logging
-import xarray as xr
-import pandas as pd
 
 import utils
-import metacsv
-from climate_toolbox import (
-    load_bcsd,
-    load_baseline,
-    weighted_aggregate_grid_to_regions)
 
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT)
@@ -29,17 +21,18 @@ logger.setLevel('DEBUG')
 
 __author__ = 'Michael Delgado'
 __contact__ = 'mdelgado@rhg.com'
-__version__ = '1.0'
+__version__ = '1.1'
 
 
 BCSD_orig_files = (
-    '/global/scratch/{read_acct}/nasa_bcsd/raw_data/{scenario}/{model}/{source_variable}/' +
+    '/global/scratch/{read_acct}/nasa_bcsd/raw_data/{scenario}/{model}/' +
+    '{source_variable}/' +
     '{source_variable}_day_BCSD_{scenario}_r1i1p1_{model}_{year}.nc')
 
 WRITE_PATH = (
-    '/global/scratch/mdelgado/web/gcp/climate/{scenario}/{agglev}/' +
-    '{variable}/' +
-    '{variable}_{frequency}_{unit}_{scenario}_{agglev}_{aggwt}_{model}_{year}.nc')
+    '/global/scratch/mdelgado/projection/gcp/climate/' +
+    '{agglev}/{aggwt}/{frequency}/{variable}/{scenario}/{model}/{year}/' +
+    '{version}.nc4')
 
 description = '\n\n'.join(
         map(lambda s: ' '.join(s.split('\n')),
@@ -56,13 +49,19 @@ ADDITIONAL_METADATA = dict(
     repo='https://github.com/ClimateImpactLab/ceci-nest-pas-une-pipe',
     file='/team_poly_bcsd.py',
     execute='python team_poly_bcsd.py run',
-    project='gcp', 
+    project='gcp',
     team='climate',
     probability_method='SMME',
-    frequency='daily')
+    frequency='daily',
+    dependencies='climate-tas-NASA_BCSD-originals.1.0')
 
-ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
-''' converts numbers into ordinal strings '''
+
+def ordinal(n):
+    ''' converts numbers into ordinal strings '''
+
+    return (
+        "%d%s" %
+        (n, "tsnrhtdd"[(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10::4]))
 
 
 def format_docstr(docstr):
@@ -81,28 +80,42 @@ def create_polynomial_transformation(power=2):
 
     powername = ordinal(power)
 
-    description = format_docstr('''
-        Daily average temperature (degrees C) raised to the {powername} power
+    description = format_docstr(('''
+            Daily average temperature (degrees C){raised}
 
-        Leap years are removed before counting days (uses a 365 day 
-        calendar). 
-        '''.format(powername=powername).strip())
+            Leap years are removed before counting days (uses a 365 day
+            calendar).
+            '''.format(
+                raised='' if power == 1 else (
+                    ' raised to the {powername} power'
+                    .format(powername=powername)))).strip())
 
-    varname = 'tas-poly-{}'.format(power)
+    varname = 'tas-poly-{}'.format(power) if power > 1 else 'tas'
 
     def tas_poly(ds):
+
+        import xarray as xr
+        import numpy as np
 
         ds1 = xr.Dataset()
 
         # remove leap years
         ds = ds.loc[{
             'time': ~((ds['time.month'] == 2) & (ds['time.day'] == 29))}]
-        
+
         # do transformation
         ds1[varname] = (ds.tas - 237.15)**power
 
+        # Replace datetime64[ns] 'time' with YYYYDDD int 'day'
+        if ds.dims['time'] > 365:
+            raise ValueError
+
+        ds1.coords['day'] = ds['time.year']*1000 + np.arange(1, len(ds.time)+1)
+        ds1 = ds1.swap_dims({'time': 'day'})
+        ds1 = ds1.drop('time')
+
         # document variable
-        ds1[varname].attrs['unit'] = 'degreesC-pow{}'.format(power)
+        ds1[varname].attrs['unit'] = 'C^{}'.format(power) if power > 1 else 'C'
         ds1[varname].attrs['oneline'] = description.splitlines()[0]
         ds1[varname].attrs['description'] = description
         ds1[varname].attrs['variable'] = varname
@@ -115,18 +128,21 @@ def create_polynomial_transformation(power=2):
         'variable': varname,
         'source_variable': 'tas',
         'transformation': tas_poly,
-        'unit': 'degreesC-pow{}'.format(power)
+        'unit': 'C^{}'.format(power) if power > 1 else 'C'
     }
 
     return transformation_spec
 
 
-JOBS = [create_polynomial_transformation(i) for i in range(3, 10)]
+JOBS = [create_polynomial_transformation(i) for i in range(1, 10)]
+
+hist = range(1981, 2006)
+proj = range(2006, 2100)
 
 PERIODS = (
-    [dict(scenario='historical', read_acct='jiacany', year=y) for y in range(1981, 2006)] +
-    [dict(scenario='rcp45', read_acct='jiacany', year=y) for y in range(2006, 2100)] +
-    [dict(scenario='rcp85', read_acct='jiacany', year=y) for y in range(1982, 2100)])
+    [dict(scenario='historical', read_acct='jiacany', year=y) for y in hist] +
+    [dict(scenario='rcp45', read_acct='jiacany', year=y) for y in proj] +
+    [dict(scenario='rcp85', read_acct='jiacany', year=y) for y in proj])
 
 MODELS = list(map(lambda x: dict(model=x), [
     'ACCESS1-0',
@@ -175,33 +191,38 @@ def run_job(
         aggwt,
         weights=None):
 
+    import xarray as xr
+    import metacsv
+
+    from climate_toolbox import (
+        load_bcsd,
+        weighted_aggregate_grid_to_regions)
+
     # Add to job metadata
     metadata.update(ADDITIONAL_METADATA)
 
-    dependencies = {}
-
-    logger.debug('Beginning job:\n\tkwargs:\t{}'.format(
-        pprint.pformat(metadata, indent=2)))
+    file_dependencies = {}
 
     read_file = BCSD_orig_files.format(**metadata)
     write_file = WRITE_PATH.format(**metadata)
-    
+
     # do not duplicate
     if os.path.isfile(write_file):
         return
 
     # Get transformed data
     fp = read_file.format(year=year)
-    
+
     with xr.open_dataset(fp) as ds:
         ds.load()
 
-    dependencies[os.path.splitext(os.path.basename(fp))[0]] = (
+    file_dependencies[os.path.splitext(os.path.basename(fp))[0]] = (
         str(ds.attrs.get('version', '1.0')))
 
     logger.debug('year {} - attempting to read file "{}"'.format(year, fp))
-    ds = (load_bcsd(ds, source_variable, broadcast_dims=('time',))
-                .pipe(transformation))
+    ds = (
+            load_bcsd(ds, source_variable, broadcast_dims=('time',))
+            .pipe(transformation))
 
     varattrs = {var: dict(ds[var].attrs) for var in ds.data_vars.keys()}
 
@@ -212,13 +233,14 @@ def run_job(
                 ds, variable, aggwt, agglev, weights=weights)
 
     # Update netCDF metadata
-    ds.attrs.update(**{k: str(v)
-        for k, v in metadata.items() if k in INCLUDED_METADATA})
+    ds.attrs.update(**{
+        k: str(v) for k, v in metadata.items() if k in INCLUDED_METADATA})
     ds.attrs.update(ADDITIONAL_METADATA)
 
     # Write output
     if not os.path.isdir(os.path.dirname(write_file)):
-        logger.debug('attempting to create_directory "{}"'
+        logger.debug(
+            'attempting to create_directory "{}"'
             .format(os.path.dirname(write_file)))
 
         os.makedirs(os.path.dirname(write_file))
@@ -226,13 +248,13 @@ def run_job(
     logger.debug('attempting to write to file "{}"'.format(write_file))
 
     attrs = dict(ds.attrs)
-    attrs['dependencies'] = dependencies
-    
+    attrs['file_dependencies'] = file_dependencies
+
     for var, vattrs in varattrs.items():
         ds[var].attrs.update(vattrs)
 
     ds.to_netcdf(write_file)
-    
+
     metacsv.to_header(
         write_file.replace('.nc', '.fgh'),
         attrs=dict(attrs),
