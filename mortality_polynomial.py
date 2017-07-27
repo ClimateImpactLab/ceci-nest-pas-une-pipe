76e2dba3246f789309bae356fbcabcb049e3e27a
jgerardsimcock <justin_simcock@yahoo.com>
2017-07-27 13:59:05 -0700

    update mort

--- mortality_polynomial.py
+++ mortality_polynomial.py
@@ -145,10 +145,13 @@ def mortality_annual(gammas_path, baseline_climate_path, gdp_data_path, ssp, eco
     betas = compute_betas(baseline_climate_path,gdp_data_path, gammas_path, ssp, econ_model)
     climate = get_annual_climate(annual_climate_paths,year, 4)
 
+    print(betas)
+    print(climate)
+
     impact = xr.Dataset()
     
     impact['mortality_impact'] = (betas['tas']*climate['tas'] + betas['tas-poly-2']*climate['tas-poly-2'] + 
-            betas['tas-poly-3']*climate['tas-poly-3'] + betas['tas-poly-3']*climate['tas-poly-3'])
+            betas['tas-poly-3']*climate['tas-poly-3'] + betas['tas-poly-4']*climate['tas-poly-4'])
 
     return impact
 
