a4e3f83a65d4d9c5e4dfa56cb57148126a9d129f
Michael Delgado <delgado.michaelt@gmail.com>
2017-07-27 10:17:22 -0700

    reshape updated to fit ceci

--- reshape.py
+++ reshape.py
@@ -1,5 +1,12 @@
+'''
+Formatted SMME "Surrogate" Synthetic Model Results
+
+Data are synthetic model outputs produced from pattern scaling 0.25-degree
+downscaled CMIP5 model outputs. Pattern scaling uses the SMME method.
+'''
 
 import os
+import utils
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -18,9 +25,35 @@ BCSD_pattern_files = (
     '{scenario}/{source_variable}/{model}/' +
     '{source_variable}_BCSD_{model}_{scenario}_r1i1p1_{{season}}_{year}.nc')
 
-season_month_start = {'DJF': 12, 'MAM': 3, 'JJA': 6, 'SON': 9}
+WRITE_PATH = (
+    '/global/scratch/mdelgado/nasa_bcsd/SMME_formatted/{scenario}/{model}/{source_variable}/' +
+    '{source_variable}_SMME-formatted_{scenario}_{model}_{year}.nc')
 
+description = '\n\n'.join(
+        map(lambda s: ' '.join(s.split('\n')),
+            __doc__.strip().split('\n\n')))
+
+oneline = description.split('\n')[0]
+
+ADDITIONAL_METADATA = dict(
+    oneline=oneline,
+    description=description,
+    author=__author__,
+    contact=__contact__,
+    version=__version__,
+    repo='https://github.com/ClimateImpactLab/ceci-nest-pas-une-pipe',
+    file='/reshape.py',
+    execute='python reshape.py run',
+    project='gcp',
+    team='climate',
+    probability_method='SMME',
+    frequency='daily',
+    dependencies='climate-tas-NASA_BCSD-originals.1.0')
+
+# Calibration data
+season_month_start = {'DJF': 12, 'MAM': 3, 'JJA': 6, 'SON': 9}
 years = range(1982, 2100)
+INVALID = 9.969209968386869e+36  # invalid data found in pattern data sets
 
 PERIODS = (
     [dict(scenario='rcp45', read_acct='mdelgado', year=y) for y in years] +
@@ -66,6 +99,13 @@ for spec in PERIODS:
         job.update(model)
         MODELS.append(job)
 
+JOB_SPEC = (MODELS, )
+
+INCLUDED_METADATA = [
+    'variable', 'source_variable', 'units', 'scenario',
+    'year', 'model', 'agglev', 'aggwt']
+
+
 def reshape_days_to_datetime(surrogate, year, season):
     return (
                 surrogate.assign_coords(
@@ -82,11 +122,22 @@ def reshape_days_to_datetime(surrogate, year, season):
                     .drop('day'))
 
 
-def get_annual_data(year, **kwargs):
+@utils.slurm_runner(filepath=__file__, job_spec=JOB_SPEC)
+def reshape_to_annual(
+        metadata,
+        variable,
+        source_variable,
+        units,
+        transformation,
+        scenario,
+        year,
+        model,
+        read_acct,
+        baseline_model,
+        agglev,
+        aggwt,
+        weights=None):
 
-    source_variable = kwargs['source_variable']
-    baseline_model = [i for i in rcp_models[kwargs['scenario']] if i['model'] == kwargs['model']][0]['baseline_model']
-    
     baseline_file = BASELINE_FILE.format(baseline_model=baseline_model, **kwargs)
 
     seasonal_baselines = {}
@@ -109,7 +160,14 @@ def get_annual_data(year, **kwargs):
 
         with xr.open_dataset(fp) as ds:
             ds.load()
-            
+
+        # drop invalid value from DataArray
+        ds[source_variable] = (
+            ds[source_variable].where(ds[source_variable] != INVALID))
+
+        msg = "value out of bounds (100) in file {}".format(fp)
+        assert (ds[source_variable].fillna(0) < 100).all(), msg
+
         patt = reshape_days_to_datetime(ds, year+(i//4), season)            
         seasonal_data.append(patt + seasonal_baselines[season])
 
@@ -117,9 +175,8 @@ def get_annual_data(year, **kwargs):
 
     ds = ds.sel(time=ds['time.year'] == year)
 
-
-    # this needs fixing!!!!!
-    # also - why does this happen?!?
-    ds[source_variable] = ds[source_variable].where(ds[source_variable] < 1e10)
-
     return ds
+
+
+if __name__ == '__main__':
+    reshape_to_annual()
