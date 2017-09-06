e8d2e0db55820d1567a8130d06b3d1557d4c9869
jgerardsimcock <justin_simcock@yahoo.com>
2017-09-06 14:08:20 -0700

    prep script for gcm

--- merge_hdd_cdd.py
+++ merge_hdd_cdd.py
@@ -21,20 +21,15 @@ __version__= '1.0'
 
 
 READ_PATH_PATTERN_COLDD = (
-    '/global/scratch/jsimcock/projection/gcp/climate/hierid/popwt/daily/' +
-    'tasmax_hdd10/{scenario}/{model}/{year}/1.0.nc4')
+    '/global/scratch/jsimcock/projection/gcp/climate/hierid/popwt/daily/coldd_agg/historical/{model}/{year}/1.0.nc4')
+    
 
 
 READ_PATH_PATTERN_HOTDD = (
-    '/global/scratch/jsimcock/projection/gcp/climate/hierid/popwt/daily/' +
-    'tasmax_cdd30/{scenario}/{model}/{year}/1.0.nc4')
+    '/global/scratch/jsimcock/projection/gcp/climate/hierid/popwt/daily/hotdd_agg/historical/{model}/{year}/1.0.nc4')
 
 
-BCSD_pattern_archive = (
-    'GCP/climate/nasa_bcsd/SMME_formatted/{scenario}/{model}/' +
-    'tasmax/*.nc')
-
-WRITE_FILE = '/global/scratch/jsimcock/projection/gcp/climate/hierid/popwt/tasmax_degree_days/{scenario}/{model}/1.0.nc4'
+WRITE_FILE = '/global/scratch/jsimcock/projection/gcp/climate/hierid/popwt/tasmax_degree_days/historical/{model}/1.0.nc4'
 
 # READ_PATH_BCSD = ( 
 #                   )
@@ -53,22 +48,22 @@ ADDITIONAL_METADATA = dict(
     execute='python {} run'.format(__file__),
     project='gcp',
     team='climate',
-    probability_method='SMME',
-    dependencies= 'GCP-climate-nasa_bcsd-SMME_formatted.1.0')
+    probability_method='BCSD',
+    )
 
 
 hotdd_agg_METADATA= dict(
-    long_title = "aggregation of Hot degree days that tasmax is greater than 30 degree celsuis in impact regions",
+    long_title = "aggregation of Hot degree days where tasmax is greater than 30 degree celsuis in impact regions",
     units = "degree days", 
-    source = "Regional aggregated hotdd from /global/scratch/jiacany/nasa_bcsd/Labor/degree_days/tasmax/rcp85/ACCESS1-0/tasmax_exceedance_degree_days_rcp85_r1i1p1_ACCESS1-0.nc", 
+    source = "Regional aggregated hotdd from /global/scratch/jiacany/nasa_bcsd/raw_data", 
                         )
 
 
 
 coldd_agg_METADATA= dict(
-                long_title = "aggregation of Cold degree days that tasmax is less than 10 degree celsuis in impact regions", 
+                long_title = "aggregation of Cold degree days where tasmax is less than 10 degree celsuis in impact regions", 
                 units = "degree days",
-                source = "Regional aggregated colddd from /global/scratch/jiacany/nasa_bcsd/Labor/degree_days/tasmax/rcp85/ACCESS1-0/tasmax_exceedance_degree_days_rcp85_r1i1p1_ACCESS1-0.nc"
+                source = "/global/scratch/jiacany/nasa_bcsd/raw_data"
                 )
 
 hierid_METADATA = dict(
@@ -82,49 +77,17 @@ time_METADATA = dict(
                 units='YYYY'
                 )
 
-SCEN = [dict(scenario='rcp45'), dict(scenario='rcp85')]
+years= range(1981, 2006)
 
-rcp_models = {
-    'rcp45':
-        list(map(lambda x: dict(model=x[0], baseline_model=x[1]), [
-            ('pattern1', 'MRI-CGCM3'),
-            ('pattern2', 'GFDL-ESM2G'),
-            ('pattern3', 'MRI-CGCM3'),
-            ('pattern4', 'GFDL-ESM2G'),
-            ('pattern5', 'MRI-CGCM3'),
-            ('pattern6', 'GFDL-ESM2G'),
-            ('pattern27', 'GFDL-CM3'),
-            ('pattern28', 'CanESM2'),
-            ('pattern29', 'GFDL-CM3'),
-            ('pattern30', 'CanESM2'),
-            ('pattern31', 'GFDL-CM3'),
-            ('pattern32', 'CanESM2')])),
 
-    'rcp85':
-        list(map(lambda x: dict(model=x[0], baseline_model=x[1]), [
-            ('pattern1', 'MRI-CGCM3'),
-            ('pattern2', 'GFDL-ESM2G'),
-            ('pattern3', 'MRI-CGCM3'),
-            ('pattern4', 'GFDL-ESM2G'),
-            ('pattern5', 'MRI-CGCM3'),
-            ('pattern6', 'GFDL-ESM2G'),
-            ('pattern28', 'GFDL-CM3'),
-            ('pattern29', 'CanESM2'),
-            ('pattern30', 'GFDL-CM3'),
-            ('pattern31', 'CanESM2'),
-            ('pattern32', 'GFDL-CM3'),
-            ('pattern33', 'CanESM2')]))}
+MODELS = list(map(lambda x: dict(model=x), [
+    'CanESM2',
+    'GFDL-CM3',
+    'GFDL-ESM2G',
+    'MRI-CGCM3'
+    ]))
 
-years = range(2006, 2100)
 
-JOB_SPEC =[]
-
-for spec in SCEN:
-    for model in rcp_models[spec['scenario']]:
-        job = {}
-        job.update(spec)
-        job.update(model)
-        JOB_SPEC.append(job)
 
 def onfinish():
     print('all done!')
@@ -144,19 +107,19 @@ def merge_future_years(job):
   write_path = WRITE_FILE.format(**job)
 
   logger.debug(
-        'attempting to read model {}, scenario {} combo'.format(job['model'], job['scenario']))
+        'attempting to read model {}, scenario {} combo'.format(job['model']))
 
   #load paths
-  paths_hdd = [READ_PATH_PATTERN_HOTDD.format(scenario=job['scenario'], model=job['model'], year=y) for y in years]
+  paths_hdd = [READ_PATH_PATTERN_HOTDD.format(model=job['model'], year=y) for y in years]
 
-  paths_cdd = [READ_PATH_PATTERN_COLDD.format(scenario=job['scenario'], model=job['model'], year=y) for y in years]
+  paths_cdd = [READ_PATH_PATTERN_COLDD.format(model=job['model'], year=y) for y in years]
 
   #open files and set index to values in years
   ds_hdd = xr.open_mfdataset(paths_hdd, concat_dim=pd.Index(years, name='time', dtype='float32'))
   ds_cdd = xr.open_mfdataset(paths_cdd, concat_dim=pd.Index(years, name='time', dtype='float32'))
 
   logger.debug(
-        'opening files for model {}, scenario {} combo'.format(job['model'], job['scenario']))
+        'opening files for model {}, scenario {} combo'.format(job['model']))
 
   #merge datasets
   merged = xr.merge([ds_cdd[ds_cdd.variable], ds_hdd[ds_hdd.variable]])
@@ -171,15 +134,15 @@ def merge_future_years(job):
   merged[ds_cdd.variable].attrs.update(coldd_agg_METADATA)
 
   logger.debug(
-        'Updating metadata and variable names datasets for model {}, scenario {} combo'.format(job['model'], job['scenario']))
+        'Updating metadata and variable names datasets for model {}, scenario {} combo'.format(job['model']))
 
   #rename variables
-  merged.rename({ds_cdd.variable: 'coldd_agg'}, inplace=True)
-  merged.rename({ds_hdd.variable: 'hotdd_agg'}, inplace=True)
-  merged.coldd_agg.attrs.update({'variable': 'coldd_agg'})
-  merged.coldd_agg.attrs.pop('description')
-  merged.hotdd_agg.attrs.update({'variable': 'hotdd_agg'})
-  merged.hotdd_agg.attrs.pop('description')
+  # merged.rename({ds_cdd.variable: 'coldd_agg'}, inplace=True)
+  # merged.rename({ds_hdd.variable: 'hotdd_agg'}, inplace=True)
+  # merged.coldd_agg.attrs.update({'variable': 'coldd_agg'})
+  # merged.coldd_agg.attrs.pop('description')
+  # merged.hotdd_agg.attrs.update({'variable': 'hotdd_agg'})
+  # merged.hotdd_agg.attrs.pop('description')
 
 
   if not os.path.isdir(os.path.dirname(write_path)):
@@ -195,8 +158,7 @@ def merge_future_years(job):
   merged.attrs.update(ADDITIONAL_METADATA)
 
   attrs = dict(merged.attrs)
-  attrs['file_dependencies'] = str([BCSD_pattern_archive.format(scenario=job['scenario'], model=job['model'], year=y) for y in years])
-
+  
   varattrs = {var: dict(merged[var].attrs) for var in merged.data_vars.keys()}
 
   metacsv.to_header(
@@ -210,7 +172,7 @@ def merge_future_years(job):
 
 if __name__ == '__main__':
 
-  for job in JOB_SPEC:
+  for job in MODELS:
     merge_future_years(job)
 
 
